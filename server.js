2f7d99ee83240d6983aa79125d6d7a542a7fcd57
Jacob Good <jgood@good-it-solutions.com>
2020-06-17 15:53:22 -0400

    same as last commit

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+const Users = require('./routes/api/users');
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
 
 dotenv.config();
 
 connectDB();
 
+app.use('/api/users', Users);
+
 app.listen(process.env.PORT || 5000, () =>
-  console.log("Server is running on port 5000")
+  console.log('Server is running on port 5000'),
 );
